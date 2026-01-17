ec7cbf3adbe1204f6cc0b4654abdf1131597b403
Wilson05478 <wilson200547@gmail.com>
2026-01-18 01:53:50 +0800

    init for Vercel

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+        proxy: {
+          '/users': {
+            target: 'https://ctflife-demo.zeabur.app',
+            changeOrigin: true,
+            secure: false,
+          },
+          '/student': {
+            target: 'https://ctflife-demo.zeabur.app',
+            changeOrigin: true,
+            secure: false,
+          },
+          '/auth': {
+            target: 'https://ctflife-demo.zeabur.app',
+            changeOrigin: true,
+            secure: false,
+          },
+          '/course_content': {
+            target: 'https://ctflife-demo.zeabur.app',
+            changeOrigin: true,
+            secure: false,
+          },
+        }
+      },
+      plugins: [react(),tailwindcss()],
+      base: process.env.VITE_BASE_PATH || '/ctfLife',
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
